4641f650a54cd18460348747221f85a91951f019
Thea <thea.mushambadze@gmail.com>
2020-01-06 16:30:57 +0400

    update

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="header">
+    <h2>{{ titleText }}</h2>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    props: ['title'],
+    setup({ title }) {
+      return { titleText: title };
+    }
+  }
+</script>
---
+++ src/components/Movie.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="movie">
+    <h2>{{ movie.Title }}</h2>
+    <div>
+      <img width="200" :alt="altText" :src="movie.Poster" />
+    </div>
+    <p>{{ movie.Year }}</p>
+  </div>
+</template>
+
+<script>
+  import { computed } from '@vue/composition-api';
+  export default {
+    name: "Movie",
+    props: ['movie'],
+    setup({ movie }) {
+      const altText = computed(() => `The movie titled: ${movie.Title}`);
+      return { altText };
+    }
+  };
+</script>
